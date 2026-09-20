de113a4d81d7f02420b869559b6dd622a5bf1e19
ranjeethagunasekaran <ranjeethagunasekaran@gmail.com>
2026-09-20 10:15:48 +0530

    LMS-2 Create book data model

---
+++ data/book.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+        id: 1,
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        category: "Programming",
+        available: true
+    },
+    {
+        id: 2,
+        title: "The Pragmatic Programmer",
+        author: "Andrew Hunt",
+        category: "Programming",
+        available: true
+    },
+    {
+        id: 3,
+        title: "Introduction to Algorithms",
+        author: "Thomas H. Cormen",
+        category: "Computer Science",
+        available: true
+    },
+    {
+        id: 4,
+        title: "Design Patterns",
+        author: "Erich Gamma",
+        category: "Software Engineering",
+        available: true
+    }
+];
+
+export default books;
